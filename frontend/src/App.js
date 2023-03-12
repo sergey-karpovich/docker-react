d8582f2f238af528e11a1a1e4d6549792e6f3c46
Sergey Karpovich <Sergey.Karpovich@gkk.de>
2023-03-12 20:50:52 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Bye there! !!!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo"/>
+//           <h1 className="App-title">Welcome</h1>
+//         </header>
+//         <p className="App-intro">
+//           Hi there!
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
